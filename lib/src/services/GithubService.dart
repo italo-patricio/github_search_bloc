da8a091731884f12d8b3dd71a5c3ed78de425e62
italo patricio <italopatriciosouza@hotmail.com>
2019-07-02 05:11:16 -0300

    trocando serviço de request http pelo dio.

--- lib/src/services/GithubService.dart
+++ lib/src/services/GithubService.dart
@@ -1,17 +1,25 @@
 import 'dart:convert';
+import 'package:dio/dio.dart';
 import 'package:github_search_bloc/src/config/Config.dart';
 import 'package:github_search_bloc/src/models/SearchResult.dart';
 import 'package:http/http.dart' as http;
 
 class GitHubService {
+  Dio _dio ;
+
+  GitHubService() {
+    _dio = Dio() ;
+    _dio.options.baseUrl = URL_API;
+  }
+
 
   Future<SearchResult> search(String term) async {
     print('Iniciando pesquisa..');
-    final response = await http.get("$URL_API/search/repositories?q=$term");
+    final response = await _dio.get("/search/repositories?q=$term");
 
     if (response.statusCode == 200) {
       // If server returns an OK response, parse the JSON.
-      return SearchResult.fromJson(json.decode(response.body));
+      return SearchResult.fromJson(response.data);
     } else {
       // If that response was not OK, throw an error.
       throw Exception('Failed to load post');
